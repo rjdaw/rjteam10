efc2d7cbfa639d8ab21522ded291a2c8b9f205ec
shakerbaker78 <bakeamym@yahoo.com>
2020-10-09 14:12:53 -0600

    replying

--- src/HelloFromRuben.java
+++ src/HelloFromRuben.java
@@ -1,5 +1,8 @@
 public class HelloFromRuben {
     public static void main(String[] args) {
+
         System.out.println("Hello from Ruben!");
+        System.out.println("Hello from Amy!");
     }
+
 }
