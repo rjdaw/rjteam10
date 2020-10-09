aef46f32b842bfa1d80a9b42a4eb85a16cdb2600
wneves <wneves@byui.edu>
2020-10-09 14:17:20 -0600

    wanderson modified it

--- src/HelloFromRuben.java
+++ src/HelloFromRuben.java
@@ -1,5 +1,6 @@
 public class HelloFromRuben {
     public static void main(String[] args) {
         System.out.println("Hello from Ruben!");
+        System.out.println("Hello from Wanderson! Como vai?")
     }
 }
