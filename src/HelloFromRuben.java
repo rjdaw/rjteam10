fd5a7041103decd72041e53252260dc9341d2c43
wneves <wneves@byui.edu>
2020-10-09 14:16:09 -0600

    wanderson modified it

--- src/HelloFromRuben.java
+++ src/HelloFromRuben.java
@@ -1,5 +1,7 @@
 public class HelloFromRuben {
     public static void main(String[] args) {
         System.out.println("Hello from Ruben!");
+        System.out.println("Brasileiros não comem churrasco de salsicha. Wanderson.");
+
     }
 }
